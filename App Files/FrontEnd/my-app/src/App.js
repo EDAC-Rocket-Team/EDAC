fde38700669547a742016c6659f4a676b6149f80
Biro Kakhi <ebrahimkakhi@gmail.com>
2022-04-21 10:57:43 +0300

    Added Diana

--- App Files/FrontEnd/my-app/src/App.js
+++ App Files/FrontEnd/my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import CarlaPage from './CarlaFuckup';
-
+import DianaPage from './Diana';
 
 function App() {
   return (
-    <div className="App">
-    <CarlaPage/> 
+    <div className="App">0
+      <CarlaPage/> 
+      <DianaPage/>
     </div>
   );
 }
