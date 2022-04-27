c71b5e8e0ec610f7fa92c6c0a74336f5a5f7d805
nascentnote <andrewshweyfete@outlook.com>
2022-04-27 15:05:10 +0300

    andrew mistakes

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -1,18 +1,25 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import './signin.css';
+import background from "../media/heartpumping.png"; 
+import { Typography, Button, Grid } from '@mui/material'
+
  
  export default function SignIn() {
      return (
+
+        <div style={{ height: '50vh', width: '50vw', backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+            
         <Box
         component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          '& > :not(style)': { m: 5, mt: 60, ml:40, width: '25ch' },
         }}
         noValidate
         autoComplete="off"
       >
-        <TextField id="Email" label="Email" variant="filled" />
+        <TextField id="Username" label="Username" variant="filled" />
 
         <TextField
           id="outlined-password-input"
@@ -20,7 +27,10 @@ import TextField from '@mui/material/TextField';
           type="password"
           autoComplete="current-password"
         />
+        
+         <Button className='Sign In'sx={{position: 'fixed', top: 75, right: 425}} variant='contained'>Sign In</Button>
 
       </Box>
+      </div>
 ) 
 }
