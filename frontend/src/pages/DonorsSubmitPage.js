a2e86fb81c1997ac0a4272cfcf702458bae5d520
Biro Kakhi <ebrahimkakhi@gmail.com>
2022-04-27 15:02:03 +0300

    uploaded grid!

--- frontend/src/pages/DonorsSubmitPage.js
+++ frontend/src/pages/DonorsSubmitPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import background from "../media/PrayingForYou.png"; 
 import { Grid, Typography, Button} from '@mui/material';
+import {useNavigate} from "react-router-dom";
 
 export default function DonorsSubmit() {
+    let navigate = useNavigate();
     return (
         <div style={{ height: '100vh', width: '100vw', backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
             <Typography sx={{fontSize: "2em", width: "40vw", ml: "58vw", pt: "25vh"}} > Thanks for submiting your form...Hold on tight we are searching for the best match for you. </Typography>
           <Grid> 
-            <Button className='Donate' sx={{position:'fixed', top: 500, right: 100}} variant='contained'>Continue</Button>
+            <Button className='Donate' sx={{position:'fixed', top: 500, right: 100}} variant='contained' onClick={()=>{navigate("/grid")}}>Continue</Button>
            </Grid> 
         </div>
     )
