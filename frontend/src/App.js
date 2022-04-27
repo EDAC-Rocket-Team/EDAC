ff5fd48dd6e404f916b69fd9f4774759868667ec
dianahnia <diana.hanania88@gmail.com>
2022-04-27 08:47:56 +0300

    First Donor signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,40 @@
+import { useState } from 'react';
 import './App.css';
-import HomeSplash from './pages/splashscreen';
+// import HomeSplash from './pages/splashscreen';
 import SignupD  from './pages/signupDonor';
+
 
 function App() {
   return (
     <div className="App">
-      <HomeSplash/>
+      <SignupD />
     </div>
   );
 }
+
+// const App = () => {
+//   // declare a new state variable for modal open
+//   const [open, setOpen] = useState(false);
+
+//   // function to handle modal open
+//   const handleOpen = () => {
+//     setOpen(true);
+//   };
+
+//   // function to handle modal close
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <div className="App">
+//       <Button variant="contained" color="primary" onClick={handleOpen}>
+//         Signup
+//       </Button>
+//       // display the modal and pass props
+//       <ModalDialog open={open} handleClose={handleClose} />
+//     </div>
+//   );
+// };
 
 export default App;
--- frontend/src/pages/signupDonor.js
+++ frontend/src/pages/signupDonor.js
@@ -1,9 +1,198 @@
+/* import React, {useState} from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+ export default function SignupD() {
+
+  return (
+    <div>
+
+    </div>
+  );
+} */
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 export default function SignupD() {
-    return (
-        //JSX starts here
-          <div>    
-          <h1>Diana wrote this!</h1>
-          </div>
-        );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      // password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+
+              {/* <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                /> 
+              </Grid>  */}
+
+              {/* </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="date"
+                  label="Choose your birthdate"
+                  type="date"
+                  defaultValue="2017-05-24"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                </Grid> */}
+
+
+              <Grid item xs={12}>
+                <div style={{
+                  margin: 'auto',
+                  display: 'block',
+                  width: 'fit-content'
+                }}>
+                  <h3>How to create Date Picker in ReactJS?</h3>
+                      <TextField
+                      id="date"
+                      label="Choose your birthdate"
+                      type="date"
+                      defaultValue="2017-05-24"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                  />
+                </div>
+              </Grid>
+
+              <Grid item xs={12}>
+                < PhoneInput
+                  country={'lb'}
+                  required
+                  // disabled
+                  fullWidth
+                  name="phone"
+                  label="Phone Number"
+                  type="tel"
+                  id="phone"
+                  // change pattern later to match that existing in Lebanon
+                  // some method in mui... similar in funcionality to "pattern"
+                  // maybe using regex
+                  // pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}"
+                /> 
+              </Grid> 
+
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+            </Grid>
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
