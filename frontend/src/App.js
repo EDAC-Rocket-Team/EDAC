8c47d2c38b40aded5d5fab1a2eef945d7308bb14
nascentnote <andrewshweyfete@outlook.com>
2022-04-27 08:10:43 +0300

    salut tout le monde je veux commercialize l'eau de mon eieux - andrew

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import HomeSplash from './pages/splashscreen';
 import SignupD  from './pages/signupDonor';
+import SignIn from './pages/signin';
 
 function App() {
   return (
     <div className="App">
-      <HomeSplash/>
+      <SignIn/> 
     </div>
   );
 }
