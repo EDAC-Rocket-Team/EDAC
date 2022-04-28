bb35f989f02f4cd3008656dc427e5aae9e613d2a
supercalifragilisticxpialidosous <cha024@live.aul.edu.lb>
2022-04-28 11:45:46 +0300

    i dont remember tbh

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -12,6 +10,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
 
 function Copyright(props) {
   return (
@@ -37,7 +36,7 @@ export default function SignIn() {
       password: data.get('password'),
     });
   };
-
+ let navigate= useNavigate ();
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -83,6 +82,7 @@ export default function SignIn() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={()=>{navigate("/grid")}}
             >
               Sign In
             </Button>
