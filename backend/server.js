f162b86a0571b85dbc43f37fb8e0fd157dc9013e
supercalifragilisticxpialidosous <cha024@live.aul.edu.lb>
2022-04-28 16:00:22 +0300

    started with mondo db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+// const mongoose= require('mongoose')
+// mongoose.connect(mongodb+srv://DianaGH:<password>@blood-donation.kt7ft.mongodb.net/test)
 const port = 5000
 
 const app = express()
