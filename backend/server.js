22e4cf752888d199769710f60693b66f4d3eb60c
dianahnia <diana.hanania88@gmail.com>
2022-05-12 14:14:16 +0300

    API post works

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,21 @@ const app = express();
 const mongoose= require('mongoose')
 const dotenv = require('dotenv').config()
 const DonorModel = require('./models/donors')
+const cors = require('cors');
 
 const port = 3001
 
 app.listen(port, () => {
     console.log(`Server started yay on ${port}`)
 })
+
+
+
+// allows us to connect to api using react
+
+app.use(express.json());
+app.use(cors());
+
 
 mongoose.connect("mongodb+srv://DianaGH:71193734mom@blood-donation.kt7ft.mongodb.net/edac?authSource=admin&replicaSet=atlas-12wwdo-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
         .then(console.log("MongoDB Connected"))
@@ -22,6 +31,14 @@ app.get("/getDonors", (req,res) => {
             res.status(200).json(result);
         }
     })
+
+app.post("/createDonor", async (req, res) => {
+  const donor = req.body; /// will be sending this from the frntend
+  const newD = new DonorModel(donor);
+  await newD.save();
+
+  res.json(donor);
+});
 
 
 })
