f1bb7296ab3ef1a59faba7ed376959d968cb7514
dianahnia <diana.hanania88@gmail.com>
2022-05-27 15:54:30 +0300

    get specific donor from db using email

--- routes/donorRoute.js
+++ routes/donorRoute.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// const dotenv = require('dotenv').config()
+// const dotenv = require("dotenv").config();
 const DonorModel = require("../models/donors");
 
-app.get("/getDonors", (req, res) => {
-  DonorModel.find({}, (err, result) => {
-    if (err) {
-      return res.status(500).json(err);
-    } else {
-      res.status(200).json(result);
-    }
-  });
+app.get("/getSpecificDonor", async (req, res) => {
+  const { email } = req.body;
+  const donor = await DonorModel.findOne({ email: email });
+  let details = {
+    firstname: donor.firstname,
+    lastname: donor.lastname,
+    email: donor.email,
+    phone: donor.phone,
+    bloodtype: donor.bloodtype,
+    alcoholpass: donor.alcoholpass,
+    drugpass: donor.drugpass,
+  };
+  res.json(details);
 });
+
+// if (err) {
+//   return res.status(500).json(err);
+// } else {
+//   // let profile = [];
+//   result.forEach((userprofile) => {
+
+//res.status(200).json(details);
 
 app.post("/createDonor", async (req, res) => {
   const donor = req.body; /// will be sending this from the frntend
