1fb3cf51c786d846f36893ae791e042582d26619
dianahnia <diana.hanania88@gmail.com>
2022-06-03 11:27:55 +0300

    Donor route updated and working

--- routes/donorRoute.js
+++ routes/donorRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// const dotenv = require("dotenv").config();
+//const dotenv = require("dotenv").config();
 const DonorModel = require("../models/donors");
+const BenModel = require("../models/ben");
+const bcrypt = require('bcrypt');
+
 
 app.get("/getSpecificDonor", async (req, res) => {
   const { email } = req.body;
@@ -19,6 +22,7 @@ app.get("/getSpecificDonor", async (req, res) => {
   res.json(details);
 });
 
+
 // if (err) {
 //   return res.status(500).json(err);
 // } else {
@@ -28,11 +32,78 @@ app.get("/getSpecificDonor", async (req, res) => {
 //res.status(200).json(details);
 
 app.post("/createDonor", async (req, res) => {
-  const donor = req.body; /// will be sending this from the frntend
-  const newD = new DonorModel(donor);
-  await newD.save();
-
-  res.json(donor);
+  try {
+    let {
+      firstname,
+      lastname,
+      email,
+      password,
+      passwordCheck,
+      birthdate,
+      address,
+      phone,
+      bloodtype,
+      alcoholpass,
+      drugpass,
+    } = req.body;
+    // validate
+    if (
+      !firstname ||
+      !lastname ||
+      !email ||
+      !password ||
+      !passwordCheck ||
+      !birthdate ||
+      !address||
+      !phone ||
+      !bloodtype ||
+      !alcoholpass ||
+      !drugpass
+    )
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    const pattern =
+      /[a-zA-Z0-9]+[.]?([a-zA-Z0-9]+)?[@][a-z]{3,9}[.][a-z]{2,5}/g;
+    const result = pattern.test(email);
+    if (!result) {
+      return res.status(400).json({ msg: "Please enter an email." });
+    }
+    const existingBenUser = await BenModel.findOne({ email: email });
+    const existingDonorUser = await DonorModel.findOne({ email: email });
+    if (existingBenUser || existingDonorUser)
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exists." });
+    if (password.length < 8)
+      return res
+        .status(400)
+        .json({ msg: "The password needs to be at least 8 characters long." });
+    if (password != passwordCheck)
+      return res
+        .status(400)
+        .json({ msg: "Enter the same password twice for verification." });
+    // if (!acknowledge)
+    //   return res
+    //     .status(400)
+    //     .json({ msg: "Please check that all information is correct!" });
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new DonorModel({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password: passwordHash,
+      birthdate,
+      address,
+      phone,
+      bloodtype,
+      alcoholpass,
+      drugpass,
+    });
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 });
 
 module.exports = app;
